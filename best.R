214c864202ce49a7c3a3328f6fda970d2c40e661
Travis Morgenstern <travis.morgenstern@gmail.com>
2015-08-01 14:38:28 -0400

    specified # of decimals for heart attack. Not sure why I don't need to do same for other two outcomes

--- best.R
+++ best.R
@@ -1,8 +1,10 @@
 #State variable is column 7
+# first part of function binds data frame info based on state input argument
+# second part finds minimum for outcome argument, returns hospital 
+
 
 library(dplyr)
 setwd('C:/Users/Travis/Documents/R programming/rprog-data-ProgAssignment3-data')
-
 
 best<- function(state,outcome){
     data<-read.csv('outcome-of-care-measures.csv', colClasses='character')
@@ -13,11 +15,12 @@ best<- function(state,outcome){
             by_state<-rbind(by_state,data[i,]) #adds all columns of data
         }
     }
+    #returns hospital with minimum outcome 
     if (outcome == 'heart attack') {
         out<- as.numeric(by_state[,11]) #convert character to numbers, get NAs
         low<-min(out, na.rm = T) 
+        low<-sprintf(low,fmt = '%#.3g') #specifies one decimal place 
         low_info<-filter(by_state,by_state[11] == low) #finds row with min value
-        
     } else if (outcome == 'heart failure') {
         out<- as.numeric(by_state[,17])
         low<-min(out, na.rm = T)
@@ -29,7 +32,7 @@ best<- function(state,outcome){
         low_info<-filter(by_state,by_state[23] == low)
         
     }
-    hospital<-low_info[2]
+    hospital<-low_info[,2]
     return(as.character(hospital))
 }
 
